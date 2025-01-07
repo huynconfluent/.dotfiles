29bbdbb34a998f6cbd2331e7ef4e85d5acbe2308
Huy Nguyen <huy.nguyen@confluent.io>
2025-01-07 08:51:23 -0600

    feat: adding blink.cmp for auto completion

---
+++ nvim/.config/nvim/lua/plugins/blink-cmp.lua
@@ -0,0 +1,37 @@
+return {
+	{
+		"saghen/blink.cmp",
+		version = "v0.*",
+
+		---@module 'blink.cmp'
+		---@type blink.cmp.Config
+		opts = {
+			keymap = {
+				preset = "default",
+			},
+
+			appearance = {
+				use_nvim_cmp_as_default = true,
+				nerd_font_variant = "mono",
+			},
+			completion = {
+				trigger = { show_on_keyword = true },
+				keyword = { range = "full" },
+				list = { selection = "auto_insert" },
+				documentation = { auto_show = true, auto_show_delay_ms = 500, treesitter_highlighting = true },
+				menu = {
+					enabled = true,
+					scrolloff = 2,
+					scrollbar = true,
+					auto_show = function()
+						return vim.bo.buftype ~= "prompt"
+							and vim.b.completion ~= false
+							and vim.bo.filetype ~= "TelescopePrompt"
+					end,
+				},
+				ghost_text = { enabled = true },
+			},
+			signature = { enabled = true },
+		},
+	},
+}
